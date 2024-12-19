49aa2f115f41fbb852429ac417dfe8fc39461166
smiqwz <mike_orion@mail.ru>
2024-12-19 23:01:10 +0300

    task2.1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout << "Hello, world!";
+#include <iostream> 
+int main() { 
+int lap = 4; 
+int engine = 254; 
+int wheels = 93; 
+int steerinWheel = 49; 
+int wind = 21; 
+int rain = 17;
+int speed = engine + wheels + steerinWheel - wind - rain; 
+std::cout << "===================\n"; 
+std::cout << "Супер гонки. Круг " << lap << "\n"; 
+std::cout << "===================\n"; 
+std::cout << "Шумахер (" << speed << ")\n"; 
+std::cout << "===================\n"; 
+std::cout << "Водитель: Шумахер\n"; 
+std::cout << "Скорость: " << speed << "\n"; 
+std::cout << "-------------------\n"; 
+std::cout << "Оснащение\n"; 
+std::cout << "Двигатель: +" << engine << "\n"; 
+std::cout << "Колеса: +" << wheels << "\n"; 
+std::cout << "Руль: +" << steerinWheel << "\n"; 
+std::cout << "-------------------\n"; 
+std::cout << "Действия плохой погоды\n"; 
+std::cout << "Ветер: -" << wind << "\n"; 
+std::cout << "Дождь: -" << rain << "\n"; 
 }
