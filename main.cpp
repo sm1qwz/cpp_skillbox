7e291b2e4a2dc0af69485bdb2a6398a6889d4d22
smiqwz <mike_orion@mail.ru>
2024-12-20 15:01:49 +0300

    task3.2

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include <iostream> 
-using namespace std;
+
 int main() { 
-int total_time, order_time, serve_time, clients;
-cout << "Добро пожаловать! Наша программа посчитает всё за вас:)" << endl;
-cout << "Сколько длится рабочая смена в минутах?" << endl;
-cin >> total_time;
-cout << "Сколько клиент делает заказ?" << endl;
-cin >> order_time;
-cout << "Сколько времени официант обслуживает клиента?" << endl;
-cin >> serve_time;
-cout << "---Идёт подсчёт---" << endl;
-clients = total_time / (order_time + serve_time);
-cout << "Ваш официант сможет обслужить " << clients << " клиентов за " << total_time << " минут рабочей смены!";
+int a = 42;
+int b = 153;
+std::cout << "a: " << a << "\n"; /* На экран будет выведено 42 */
+std::cout << "b: " << b << "\n"; /* На экран будет выведено 153 */
+
+std::swap(a, b); //способ использования через swap
+
+// еще мой вариант через временное хранилище, выгодно ли так делать?
+//int buff = a;
+//a = b;
+//b = buff;
+
+std::cout << "a: " << a << "\n"; /* На экран будет выведено 153 */
+std::cout << "b: " << b << "\n"; /* На экран будет выведено 42 */
 }
